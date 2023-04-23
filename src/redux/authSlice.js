e9dde825616b9cc12939caaf9ae1907f928e56b6
Bisrat Kebere <kiyyakebere@gmail.com>
2023-04-24 02:04:08 +0300

    :sparkles: Add Local Storage Login
    user can logi based on past info

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -3,19 +3,22 @@ import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    user: null,
+    user: JSON.parse(window.localStorage.getItem('user')) || null,
     loading: false,
     error: null,
   },
   reducers: {
     register: (state, action) => {
       state.user = action.payload;
+      window.localStorage.setItem('user', JSON.stringify(action.payload));
     },
     login: (state, action) => {
       state.user = action.payload;
+      window.localStorage.setItem('user', JSON.stringify(action.payload));
     },
     logout: (state) => {
       state.user = null;
+      window.localStorage.removeItem('user');
     }
   },
 });
