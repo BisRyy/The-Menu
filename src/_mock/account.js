027ae635a149cd2d660f23749e8054607a0fda59
Bisrat Kebere <kiyyakebere@gmail.com>
2023-04-24 01:51:05 +0300

    :wastebasket: Files Clean Up
    Remove unnecessary template files

--- src/_mock/account.js
+++
@@ -1,9 +0,0 @@
-// ----------------------------------------------------------------------
-
-const account = {
-  displayName: 'Bisrat Kebere',
-  email: 'dev@bisrat.tech',
-  photoURL: '/assets/images/avatars/avatar_default.jpg',
-};
-
-export default account;
