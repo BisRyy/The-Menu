411a1afe643a67533210912b4049e401750b32e7
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-04-27 09:38:33 +0300

    :bug: Update Profile picture

--- src/data/account.js
+++ src/data/account.js
@@ -3,7 +3,7 @@
 const account = {
   displayName: JSON.parse(localStorage.getItem('user'))?.name,
   email: JSON.parse(localStorage.getItem('user'))?.contact.email,
-  photoURL: '/assets/images/avatars/avatar_default.jpg',
+  photoURL: JSON.parse(localStorage.getItem('user'))?.images[0] || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbKhRC-7ifkW9gq6QIFPgjOBYJniQcnaOxVneJZ1N9&s',
 };
 
 export default account;
